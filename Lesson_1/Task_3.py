cac49d7763399b19d49514db928650f2e40f13fd
Sidenth <nautilus.psihodelic@yandex.ru>
2023-03-29 21:32:58 +0900

    Lesson 1

---
+++ Lesson_1/Task_3.py
@@ -0,0 +1,9 @@
+# Задание 3.
+# Узнайте у пользователя целое положительное число n.
+# Найдите сумму чисел n + nn + nnn.
+# Пример:
+# Введите число n: 3
+# n + nn + nnn = 369
+given_digit = int(input("Введите целое положительное число: "));
+sum_digits = str(given_digit) + str(given_digit + given_digit) + str(given_digit + given_digit + given_digit);
+print(sum_digits);
