7f1b056b995f1277125cb81df7d4aea4b593d8e4
Sidenth <nautilus.psihodelic@yandex.ru>
2023-04-01 11:00:58 +0900

    fixed mistakes

--- Lesson_1/Task_3.py
+++ Lesson_1/Task_3.py
@@ -4,6 +4,8 @@
 # Пример:
 # Введите число n: 3
 # n + nn + nnn = 369
-given_digit = int(input("Введите целое положительное число: "));
-sum_digits = str(given_digit) + str(given_digit + given_digit) + str(given_digit + given_digit + given_digit);
-print(sum_digits);
+# given_digit = int(input("Введите целое положительное число: "))
+# sum_digits = str(given_digit) + str(given_digit + given_digit) + str(given_digit + given_digit + given_digit)
+# print(sum_digits)
+n = input("Введите число: ")
+print(f'Результат: {int(n) + int(n+n) + int(n+n+n)} ')
